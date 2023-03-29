f221233f045aafb839d6d9cf15a6acdec387af1d
Faizan Hamid <faizanhamid162@gmail.com>
2023-03-29 22:29:35 +0530

    navbar

--- snapdeal/src/App.js
+++ snapdeal/src/App.js
@@ -1,12 +1,14 @@
 
 import "./App.css";
 import { Navbar } from "./components/Navbar";
+
 import AllRoutes from "./components/AllRoutes";
+// import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Snapdeal</h1>
+      {/* <h1>Welcome to Snapdeal</h1> */}
       <Navbar />
       <AllRoutes />
     </div>
--- snapdeal/src/components/Navbar.jsx
+++ snapdeal/src/components/Navbar.jsx
@@ -1,9 +1,29 @@
-
-
 import React from 'react'
+import './Navbar.css'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div >
+
+     <div id='navbar1' >
+
+          <div>
+           Brand Waali Quality, Bazaar Waali Deal!
+          </div>
+        <div style={{display:'flex',justifyContent:'space-around'}} id='navbar'>
+          <p>Impact@Fashiondeal</p>
+          <p> Gift Cards </p>
+          <p> Help Center</p>
+          <p>Sell On Fashiondeal </p>
+          <p>Download App</p>
+        </div>
+
+     </div>
+
+      <div style={{border:'1px solid green',height:'60px'}}>
+        
+      </div>
+    </div>
+   
   )
 }
