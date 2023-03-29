a7738c43e21db46f48eadb69bc47529cf5d410ae
Faizan Hamid <faizanhamid162@gmail.com>
2023-03-29 11:55:28 +0530

    s

--- snapdeal/src/App.js
+++ snapdeal/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Snapdeal</h1>
+      <Navbar/>
     </div>
   );
 }
