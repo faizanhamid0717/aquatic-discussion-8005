5a069d63003456251f3ac0ea9a80c09214979291
sohel <mr.sohelsyed@gmail.com>
2023-04-01 17:15:38 +0530

    added ui and components for admin page

--- snapdeal/src/Pages/Admin.jsx
+++ snapdeal/src/Pages/Admin.jsx
@@ -1,14 +1,38 @@
-import React from "react";
-import { Box, Flex, HStack, Input, Text } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  IconButton,
+  Image,
+  Input,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 import AddLoginForm from "../components/AddLoginForm";
+import { useDispatch, useSelector } from "react-redux";
+import { getProductFn } from "../redux/productReducer/action";
+import { Search2Icon, DeleteIcon, EditIcon } from "@chakra-ui/icons";
 
 const Admin = () => {
+  const state = useSelector((state) => {
+    return state.productReducer.product;
+  });
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProductFn());
+  }, [dispatch]);
+  console.log(state);
   return (
     <Box>
       <Flex border="1px solid black" p="12px 20px" align="center">
         <Text p="12px 20px">Admin Navbar</Text>
         <Input type="search" placeholder="Search Product.." size="md" w="40%" />
-        {/* <IconButton aria-label="Search database" icon={<SearchIcon />} /> */}
+
+        <IconButton aria-label="Search database" icon={<Search2Icon />} />
       </Flex>
 
       <HStack>
@@ -16,10 +40,53 @@ const Admin = () => {
           <Text>Side Panel</Text>
           <AddLoginForm />
         </Flex>
-        <Flex w="75%" border="1px solid black" height="70vh">
+        <Box
+          w="75%"
+          m="auto"
+          border="1px solid black"
+          height="70vh"
+          overflowY="auto"
+          css={{
+            "&::-webkit-scrollbar": {
+              width: "4px",
+            },
+            "&::-webkit-scrollbar-track": {
+              width: "6px",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              borderRadius: "24px",
+            },
+          }}
+        >
           <Text>Main Box</Text>
-          <Input scr="" alt="" />
-        </Flex>
+
+          <SimpleGrid columns={5} spacing={3} w="100%" align="center">
+            {state.length > 0 &&
+              state.map((el) => {
+                return (
+                  <Box border="1px solid black" w="190px">
+                    <Image src={el.image} />
+                    <Text>{el.title}</Text>
+
+                    <IconButton
+                      colorScheme="blue"
+                      aria-label="edit Product"
+                      size="xs"
+                      icon={<EditIcon />}
+                      w={9}
+                    />
+                    <IconButton
+                      colorScheme="red"
+                      aria-label="delete Product"
+                      size="xs"
+                      icon={<DeleteIcon />}
+                      w={9}
+                    />
+                  </Box>
+                );
+              })}
+          </SimpleGrid>
+        </Box>
       </HStack>
     </Box>
   );
--- snapdeal/src/components/AddLoginForm.jsx
+++ snapdeal/src/components/AddLoginForm.jsx
@@ -7,6 +7,8 @@ import {
   DrawerOverlay,
   DrawerCloseButton,
   DrawerHeader,
+  Stack,
+  Input,
 } from "@chakra-ui/react";
 import React from "react";
 
@@ -15,7 +17,13 @@ export default function AddLoginForm() {
 
   return (
     <>
-      <Button onClick={onOpen} m={4}>
+      <Button
+        onClick={onOpen}
+        w="100%"
+        m="10px"
+        colorScheme="blue"
+        color="white"
+      >
         ADD PRODUCT
       </Button>
 
@@ -23,8 +31,21 @@ export default function AddLoginForm() {
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
-          <DrawerHeader>ADD PRODUCT</DrawerHeader>
-          <DrawerBody>{/* add Product from hare */}</DrawerBody>
+          <DrawerHeader>ADD Product</DrawerHeader>
+          <DrawerBody>
+            {/* add Product from hare */}
+            <Stack>
+              <Input placeholder="Enter Product Name" size="md" />
+              <Input placeholder="Enter Product Imange URL" size="md" />
+              <Input placeholder="Enter Product Price" size="md" />
+              <Input placeholder="Enter Product Discount" size="md" />
+              <Input placeholder="Enter Product Star" size="md" />
+              <Input placeholder="Enter Product DisPrice" size="md" />
+              <Button colorScheme="blue" color="white">
+                ADD
+              </Button>
+            </Stack>
+          </DrawerBody>
         </DrawerContent>
       </Drawer>
     </>
